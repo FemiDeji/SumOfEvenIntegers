608d483bc64357853473d7b7042d903a54b3449d
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:55:17 -0800

    Initial commit

---
+++ src/SumOfEvenIntegers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SumOfEvenIntegers {
+    public static void main(String[] args) {
+
+        //this program is to print the sum of 3 even integers between 0 and 6
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter even numbers here");
+        p= sum;
+        int even = sc.nextInt();
+        int i;
+        for(int i = 0; i < sum; i++) {
+            for (int j =0; j < sum; j++ ) {
+
+
+            }
+
+    }
+}
